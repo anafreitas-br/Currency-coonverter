ebe26723c596d737879c4ebdc4390585761f4321
Ana Freitas <rm565559@fiap.com.br>
2025-08-16 08:56:25 -0300

    final commit: currency-converter

---
+++ src/main.ts
@@ -0,0 +1,41 @@
+import "./style.css";
+import { rates } from "./data/rates";
+import type { Currency } from "./types";
+import convert from "./convert";
+
+
+const fromSelect = document.getElementById("from") as HTMLSelectElement;
+const toSelect = document.getElementById("to") as HTMLSelectElement;
+
+function populateSelect(select: HTMLSelectElement) {
+  Object.keys(rates).forEach((currency) => {
+    const option = document.createElement("option");
+    option.value = currency;
+    option.textContent = currency;
+    select.appendChild(option);
+  });
+}
+
+populateSelect(fromSelect);
+populateSelect(toSelect);
+fromSelect.value = "USD";
+toSelect.value = "BRL";
+
+
+const amountInput = document.getElementById("amount") as HTMLInputElement;
+const convertBtn = document.getElementById("convert") as HTMLButtonElement;
+const result = document.getElementById("result") as HTMLParagraphElement;
+
+convertBtn.addEventListener("click", () => {
+  const amount = parseFloat(amountInput.value);
+  const from = fromSelect.value as Currency;
+  const to = toSelect.value as Currency;
+
+  if (isNaN(amount) || amount <= 0) {
+    result.textContent = "Por favor, insira um valor válido";
+    return;
+  }
+
+  const converted = convert(amount, from, to);
+  result.textContent = `${amount} ${from} = ${converted} ${to}`;
+});
